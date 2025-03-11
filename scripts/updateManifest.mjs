4cf640d787f135c8a69c0060f65cedf3c84636f5
Justin Tieu <justin@provusinc.com>
2025-03-10 22:22:04 -0700

    fix: removed env check for update manifest script

--- scripts/updateManifest.mjs
+++ scripts/updateManifest.mjs
@@ -1,33 +1,22 @@
 import fs from "fs";
 import path from "path";
-import { config } from "dotenv";
 
-// Load environment variables from the .env file
-config();
+const packageJsonPath = path.join(process.cwd(), "package.json");
+const manifestPath = path.join(process.cwd(), "manifest.json");
 
-// Access the environment variable
-const shouldUpdateManifest = process.env.UPDATE_MANIFEST_VERSION === "true";
+// Read the version from package.json
+const packageJson = JSON.parse(fs.readFileSync(packageJsonPath, "utf8"));
+const newVersion = packageJson.version;
 
-if (shouldUpdateManifest) {
-    const packageJsonPath = path.join(process.cwd(), "package.json");
-    const manifestPath = path.join(process.cwd(), "manifest.json");
+// Read the current manifest.json
+const manifest = JSON.parse(fs.readFileSync(manifestPath, "utf8"));
 
-    // Read the version from package.json
-    const packageJson = JSON.parse(fs.readFileSync(packageJsonPath, "utf8"));
-    const newVersion = packageJson.version;
+// Set the version in manifest.json to match package.json
+manifest.version = newVersion;
 
-    // Read the current manifest.json
-    const manifest = JSON.parse(fs.readFileSync(manifestPath, "utf8"));
+// Write the updated manifest.json back to disk
+fs.writeFileSync(manifestPath, JSON.stringify(manifest, null, 2));
 
-    // Set the version in manifest.json to match package.json
-    manifest.version = newVersion;
-
-    // Write the updated manifest.json back to disk
-    fs.writeFileSync(manifestPath, JSON.stringify(manifest, null, 2));
-
-    console.log(
-        `Updated manifest.json version to match package.json version: ${newVersion}`,
-    );
-} else {
-    console.log("Skipping manifest.json update.");
-}
+console.log(
+    `Updated manifest.json version to match package.json version: ${newVersion}`,
+);
